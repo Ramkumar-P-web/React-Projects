4fe02d9f1e9dd024b6f5554ae68a18d73523a2bb
Ramkumar-P-web <p.ramkumar44ram@gmail.com>
2025-06-15 23:57:06 +0530

    Working on Applications Page

--- Job Portal/client/src/pages/Applications.jsx
+++ Job Portal/client/src/pages/Applications.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Loading from '../components/Loading'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 const Applications = () => {
+
+  const [isEdit,setIsEdit] = useState(false);
+
   return ( true ? (
     <>
      <Navbar/>
+     <div className='container px-4 2xl:px-20 mx-auto min-h-[65vh] my-10'>
+     <h2>Your Resume</h2>
      <div>
+      {
+        isEdit ? (
+          <>
+          </>
+        ) : (
+          <div>
+
+          </div>
+        )
+      }
+     </div>
 
      </div>
      <Footer></Footer>

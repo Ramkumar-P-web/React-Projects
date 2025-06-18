009c58c58c8e11e740fa1faf7f374e901543fc3f
Ramkumar-P-web <p.ramkumar44ram@gmail.com>
2025-06-18 22:26:38 +0530

    Basic Dependencies intalled and Basice server creaated with Express

--- Job Portal/server/server.js
+++ Job Portal/server/server.js
@@ -1,4 +1,20 @@
-const express = require('express');
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+//Intialize Express
 const app = express();
 
-app.listen(5000,()=>{console.log('Server listening on Port 5000')});
+//MiddleWares
+app.use(cors());
+app.use(express.json())
+
+//Routes
+app.get('/',(req,res)=>{
+   res.send("API Woring")
+})
+
+//PORT
+const PORT = process.env.PORT || 5000 ;
+
+app.listen(PORT,()=>{console.log(`Server is running on Port ${PORT}`)});
